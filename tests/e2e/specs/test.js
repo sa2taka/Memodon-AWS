d2263bfa585ce71e030640994b5eef30aed776ac
sa2taka <sa2taka@gmail.com>
2019-09-19 07:15:19 +0000

    :rotating_light: Add simple test to confirm correct behavior

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,9 @@
+/* global cy*/
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Memodon is moving correct', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.visit('/');
+    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App');
   })
 })
